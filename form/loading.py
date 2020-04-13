b4cd89a131b0b42e046702006a560887afa366a9
romaricano <58915209+romaricano@users.noreply.github.com>
2020-04-13 12:28:46 +0000

    adding loading.py

---
+++ form/loading.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'loading.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(628, 468)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(10, 20, 601, 401))
+        self.groupBox.setStyleSheet("Border-radius: 10px;\n"
+"background-color: rgb(218, 218, 218);")
+        self.groupBox.setObjectName("groupBox")
+        self.frame = QtWidgets.QFrame(self.groupBox)
+        self.frame.setGeometry(QtCore.QRect(130, 80, 331, 221))
+        self.frame.setStyleSheet("background-color: rgb(127, 127, 127);\n"
+"border-radius: 15px;")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(10, 40, 111, 51))
+        self.label.setObjectName("label")
+        self.progressBar = QtWidgets.QProgressBar(self.frame)
+        self.progressBar.setGeometry(QtCore.QRect(47, 120, 241, 31))
+        self.progressBar.setStyleSheet("border-radius: 5px;\n"
+"background-color: rgb(255, 255, 255);")
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName("progressBar")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Hospital"))
+        self.groupBox.setTitle(_translate("MainWindow", "Ouverture de session"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:16pt; font-weight:600;\">Loading...</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
